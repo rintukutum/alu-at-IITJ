ed111de1b204f0aefd219e251a17d9dd3a17a5a4
rintukutum <rintukutum@gmail.com>
2022-04-11 19:16:41 +0530

    updated

--- 00-data-format.R
+++ 00-data-format.R
@@ -91,27 +91,34 @@ colnames(alu_whole) <- c(
   'chr','alu.start','alu.end','alu.type',
   'ucsc.score','strand'
 )
+
+idx.rm <- grep('\\_ ',alu_whole$chr)
+
+alu_whole_chrs <- alu_whole[-idx.rm,]
 alu_grange <- GRanges(
-  seqnames = Rle(alu_whole$chr),
-  ranges = IRanges(start = alu_whole$alu.start,
-                   end = alu_whole$alu.end),
-  strand = strand(Rle(alu_whole$strand))
+  seqnames = Rle(alu_whole_chrs$chr),
+  ranges = IRanges(start = alu_whole_chrs$alu.start,
+                   end = alu_whole_chrs$alu.end),
+  strand = strand(Rle(alu_whole_chrs$strand))
 )
 
 load('data/jm.RData')
+
+table(alu_whole_chrs[,c('chr','strand')])
+
 
 
 # barplot(sort(table(alu_whole$chr),
 #              decreasing = TRUE)[1:10],
 #              horiz = TRUE,
 #         las=2)
-
+alu_whole <- alu_whole_chrs
 idx_chr1 <- alu_whole$chr == 'chr1'
 alu_chr1 <- alu_whole[idx_chr1,]
 
-# barplot(sort(table(alu_chr$alu.type)),
-#         horiz = TRUE,
-#         las=2)
+barplot(sort(table(alu_chr1$alu.type)),
+        horiz = TRUE,
+        las=2)
 
 chr1_alu_types <- unique(alu_chr1$alu.type)
 
@@ -124,17 +131,81 @@ loop <- 1:n_chr1_alu_types
 # }
 par(mfrow=c(2,2))
 for(index in 1:4){
-  alu_type <- alu_types[index]
-  idx_alu <- alu_chr$alu.type == alu_type
-  alu_score <- alu_chr[idx_alu,]
-  boxplot(alu_score$ucsc.score~alu_score$strand,
-          ylab='Score',main= alu_type,
-          xlab='Strand')
+  alu_type <- chr1_alu_types[index]
+  idx_alu <- alu_chr1$alu.type == alu_type
+  alu_score <- alu_chr1[idx_alu,]
+  #-------
+  boxplot(
+    alu_score$ucsc.score ~ alu_score$strand,
+    ylab ='Score',
+    main = alu_type,
+    xlab ='Strand',
+    col  = c('red','skyblue')
+  )
 }
 
 library(ggplot2)
 ggplot(data = alu_chr1,
-       aes(x = strand,y=log10(ucsc.score))) +
+       aes(x = strand,
+           y = log10(ucsc.score))) +
   geom_boxplot() +
   facet_wrap(facets = 'alu.type')
+#-----------
+# distribution of alu
+alu_type_chr <- data.frame(table(alu_whole[,c('chr','alu.type','strand')]))
+ggplot(alu_type_chr,aes(x=chr,y=alu.type)) +
+  geom_tile(aes(fill=log10(Freq+1))) +
+  scale_fill_gradient2() +
+  theme(axis.text.x = element_text(
+    angle=45,vjust = 1,hjust = 1))
 
+library(Homo.sapiens)
+library(ggbio)
+library(biovizBase)
+# p <- ggbio() + circle(
+#   jm_grange,geom='rect', aes(color = strand)) +
+#   circle(hg19sub, geom = "ideo", fill = "gray70") +
+#   circle(hg19sub, geom = "scale", size = 2) + 
+#   circle(hg19sub, geom = "text", aes(label = seqnames), vjust = 0, size = 3)
+# 
+# #-------
+# pos_strand <- jm_grange[jm_grange@strand@values == '+',]
+# neg_strand <- jm_grange[jm_grange@strand@values == '-',]
+# p <- ggbio() + 
+#   circle(pos_strand,geom='rect', color = 'steelblue') +
+#   circle(neg_strand,geom='rect', color = 'red') +
+#   circle(hg19sub, geom = "ideo", fill = "gray70") +
+#   circle(hg19sub, geom = "scale", size = 2) + 
+#   circle(hg19sub, geom = "text", aes(label = seqnames), vjust = 0, size = 3)
+
+#--------
+jaspar_mef2c <- readr::read_tsv(
+  'data/Jaspar_MEF2C',
+  col_names = FALSE
+)
+colnames(jaspar_mef2c) <- c(
+  'chr','start','end','info','dot','strand'
+)
+jaspar_mef2c <- jaspar_mef2c[-grep('X',jaspar_mef2c$chr),]
+# vector
+jm_start <- jaspar_mef2c$start
+jm_end <- jaspar_mef2c$end
+jm_irange <- IRanges(start = jm_start, end = jm_end)
+jm_chrs <- jaspar_mef2c$chr
+jm_chrs <- gsub('chr','',jm_chrs)
+jm_strand <- jaspar_mef2c$strand
+
+jm_g <- GRanges(
+  seqnames = Rle(jm_chrs),
+  ranges = jm_irange,
+  strand = strand(jm_strand)
+)
+genome(jm_g) <- 'hg19'
+pos_s <- jm_g[jm_g@strand@values == '+',]
+pos_s <- jm_g[jm_g@strand@values == '-',]
+p <- ggbio() + 
+  circle(hg19sub, geom = "ideo", fill = "gray70") +
+  circle(hg19sub, geom = "scale", size = 2) + 
+  circle(hg19sub, geom = "text", aes(label = seqnames), vjust = 0, size = 3) +
+  circle(pos_s, geom='rect', color = 'steelblue') +
+  circle(pos_s, geom='rect', color = 'red')
---
+++ 01-data-clean.R
@@ -0,0 +1,73 @@
+#' ref https://ggplot2-book.org/index.html
+rm(list=ls())
+datasets <- list.files(path = 'data/', pattern = 'DATASET', full.names = TRUE)
+
+alu_mef2c <- readr::read_csv(datasets[5])
+alu_mef2c_exons <- readr::read_csv(datasets[1])
+alu_mef2c_ig <- readr::read_csv(datasets[2])
+alu_mef2c_intron <- readr::read_csv(datasets[3])
+alu_mef2c_promo <- readr::read_csv(datasets[4])
+total <- nrow(alu_mef2c_exons) + nrow(alu_mef2c_ig) +
+  nrow(alu_mef2c_intron) + nrow(alu_mef2c_promo)
+
+alu_freq <- c(
+  'exons' = nrow(alu_mef2c_exons),
+  'intergenic' = nrow(alu_mef2c_ig),
+  'intron' = nrow(alu_mef2c_intron),
+  'promoter' = nrow(alu_mef2c_promo))
+
+alu.prop <- round(c('exons' = nrow(alu_mef2c_exons)/total,
+  'intergenic' = nrow(alu_mef2c_ig)/total,
+  'intron' = nrow(alu_mef2c_intron)/total,
+  'promoter' = nrow(alu_mef2c_promo)/total
+  )*100,2)
+
+pie(alu.prop)
+
+#-------
+non_alu_mef2c <- readr::read_csv(datasets[10])
+non_alu_mef2c_exons <- readr::read_csv(datasets[6])
+non_alu_mef2c_ig <- readr::read_csv(datasets[7])
+non_alu_mef2c_intron <- readr::read_csv(datasets[8])
+non_alu_mef2c_promo <- readr::read_csv(datasets[9])
+non_total <- nrow(non_alu_mef2c_exons) + nrow(non_alu_mef2c_ig) +
+  nrow(non_alu_mef2c_intron) + nrow(non_alu_mef2c_promo)
+
+
+non_alu_freq <- c('exons' = nrow(non_alu_mef2c_exons),
+              'intergenic' = nrow(non_alu_mef2c_ig),
+              'intron' = nrow(non_alu_mef2c_intron),
+              'promoter' = nrow(non_alu_mef2c_promo))
+non_alu.prop <- round(c('exons' = nrow(non_alu_mef2c_exons)/non_total,
+                    'intergenic' = nrow(non_alu_mef2c_ig)/non_total,
+                    'intron' = nrow(non_alu_mef2c_intron)/non_total,
+                    'promoter' = nrow(non_alu_mef2c_promo)/non_total
+)*100,2)
+
+pdf('figures/Alu-MEF2C.pdf', width = 5, height = 5)
+pie(alu.prop, main = 'Alu with MEF2C')
+dev.off()
+
+png('figures/Alu-MEF2C.png', 
+    width = 800, height = 700, 
+    res = 150)
+pie(alu.prop, main = 'Alu with MEF2C')
+dev.off()
+
+pie(alu.prop, main = 'Alu with MEF2C')
+dev.off()
+
+pdf('figures/Alu-MEF2C.pdf',width = 5, height = 5)
+pie(non_alu.prop, main = 'Non-Alu with MEF2C')
+dev.off()
+
+names(non_alu.prop) <- paste0(names(non_alu.prop),'(',non_alu.prop,'%)')
+png('figures/non-Alu-MEF2C.png', 
+    width = 800, height = 700, 
+    res = 150)
+pie(non_alu.prop, main = 'Non-Alu with MEF2C')
+dev.off()
+
+prop.test(alu_freq, non_alu_freq)$p.value
+
+
